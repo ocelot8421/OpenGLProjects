f79db1935d5d33c1db22aca0c6decc9c5f98ca1a
ocelo8421 <8ocelot@gmail.com>
2023-06-08 21:20:49 +0200

    finished

--- src/main.cpp
+++ src/main.cpp
@@ -5,148 +5,196 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <cmath>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);				//Adjusts the viewport to the window if the user resizes it
-void processInput(GLFWwindow *window);													//Checks inputs
-std::string loadShaderSrc(const char* filename);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+std::string loadShaderSrc(const char *filename);
 
-int main() {
+int main()
+{
 	std::cout << "main starting..." << std::endl;
 
-	int success;															//for exeption handling of vertex shader
+	float widthWindow = 900;
+	float heightWindow = 600;
+	float margin = 10;
+	float widthView = widthWindow - 2 * margin;
+	float heightView = heightWindow - 2 * margin;
+
+	char markers[] = {'a',
+					  'b',
+					  'c'};
+	float sides[3];
+
+	for (int i = 0; i < std::size(markers); i++)
+	{
+		std::cout << "side " << markers[i] << ":" << std::endl;
+		float s;
+		std::cin >> s;
+		sides[i] = s;
+	}
+
+	float a = sides[0];
+	float b = sides[1];
+	float c = sides[2];
+	float delta = 0;
+
+	int success;
 	char infoLog[512];
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);										// openGL version 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);						//use openGL core-profile
-    GLFWwindow* window = glfwCreateWindow(900, 600, "First steps in Neuroimaging", NULL, NULL);		// create a window (on Windows OS), params: width, height, title, monitor specifications? //TODO (0) monitor specifications?
-    if (window == NULL) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    } 
-    glfwMakeContextCurrent(window);
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {				//Checks if Glad is loaded
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-	glViewport(0, 0, 900, 600);												//rendering window with its dimensions (lower left corner's coordinates, width, height)
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);		//Adjusts the viewport to the window if the user resizes it
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow *window = glfwCreateWindow(widthWindow, heightWindow, "edag", NULL, NULL); // create a window (on Windows OS), params: width, height, title, monitor specifications? //TODO (0) monitor specifications?
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(margin, margin, widthView, heightView);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	/*
-		shaders																//TODO understand what it does exactly do.
-	*/																		// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+		shaders
+	*/
 
-	//compile vertex shader
-	 unsigned int vertexShader;
-	 vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	 std::string vertShaderSrc = loadShaderSrc("../assets/vertex_core.glsl");						// ../ because of bin file
-	 std::cout << "vertShaderSrc: " << vertShaderSrc << std::endl; //TODO ---
-	 const GLchar* vertShader = vertShaderSrc.c_str();
-	 glShaderSource(vertexShader, 1, &vertShader, NULL);
-	 glCompileShader(vertexShader);
-	 //catch errors
+	// compile vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	std::string vertShaderSrc = loadShaderSrc("../assets/vertex_core.glsl");
+	const GLchar *vertShader = vertShaderSrc.c_str();
+	glShaderSource(vertexShader, 1, &vertShader, NULL);
+	glCompileShader(vertexShader);
+	// catch errors
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success){
+	if (!success)
+	{
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "Error width vertex shader comp.: " << std::endl << infoLog << std::endl;
-	}
-	
-	//compile fragment shader
-	 unsigned int fragmentShader;
-	 fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	 std::string fragShaderSrc = loadShaderSrc("../assets/fragment_core.glsl");						// ../ because of bin file
-	 std::cout << "fragShaderSrc: " << fragShaderSrc << std::endl; //TODO ---
-	 const GLchar* fragShader = fragShaderSrc.c_str();
-	 glShaderSource(fragmentShader, 1, &fragShader, NULL);
-	 glCompileShader(fragmentShader);
-	 //catch errors
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success){
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "Error width fragment shader comp.: " << std::endl << infoLog << std::endl;
+		std::cout << "Error width vertex shader comp.: " << std::endl
+				  << infoLog << std::endl;
 	}
 
-	//link
+	// compile fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	std::string fragShaderSrc = loadShaderSrc("../assets/fragment_core.glsl");
+	const GLchar *fragShader = fragShaderSrc.c_str();
+	glShaderSource(fragmentShader, 1, &fragShader, NULL);
+	glCompileShader(fragmentShader);
+	// catch errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Error width fragment shader comp.: " << std::endl
+				  << infoLog << std::endl;
+	}
+
+	// link
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader); 
+	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
-	 //catch errors
+	// catch errors
 	glGetShaderiv(shaderProgram, GL_COMPILE_STATUS, &success);
-	if (!success){
+	if (!success)
+	{
 		glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "Linking error: " << std::endl << infoLog << std::endl;
+		std::cout << "Linking error: " << std::endl
+				  << infoLog << std::endl;
 	}
 
 	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);											// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-	/* shaders*/															//TODO understand what it does exactly do.
+	glDeleteShader(fragmentShader);
+	/* shaders*/
 
-	//vertex array
-	float vertices[]  = {
-		-0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f
-	};
+	float a_b = (b * b - a * a - c * c) / (-2 * a); // b oldal vetülete a-ra (nominális)
+	float a_c = -a + a_b;							// c oldal vetülete a-ra
+	float mA = std::sqrt(c * c - a_b * a_b);
+
+	float Bx = a_b / widthView;
+	float Ax = a_c / widthView;
+
+	float Cy = mA / 2 / heightView;
+	float By = -Cy;
+	float Ay = By;
+
+	// vertex array
+	float vertices[] = {
+		Bx, 0.0f, 0.0f,
+		Ax, 0.0f, 0.0f,
+		0.0f, Cy, 0.0f};
+
+	std::cout << "Bx: " << Bx << " -- Ax: " << Ax << " -- mA: " << mA << " -- Cy: " << Cy << std::endl;
 	// VAO, VBO
 	unsigned int VAO;
 	unsigned int VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	//bind VAO
+	// bind VAO
 	glBindVertexArray(VAO);
-	//bind VBO
+	// bind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	// set attribute pointer
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
 
-
-    while (!glfwWindowShouldClose(window)) {								//RENDER LOOP (double buffer)
-		processInput(window);												// takes the window as input together with a key
-		//RENDERING COMMANDS
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+		// RENDERING COMMANDS
 		glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		// draw shapes
 		glBindVertexArray(VAO);
 		glUseProgram(shaderProgram);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        glfwSwapBuffers(window);											//Swaps buffer that contains color values for each pixel in GLFW's window
-        glfwPollEvents();													//checks keyboard input or mouse movement events, updates the window state, and calls the corresponding functions
-    }
-    glfwTerminate();														//clean,delete all of GLFW's resources
-    return 0;
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){	//Adjusts the viewport to the window if the user resizes it
-    glViewport(0, 0, width, height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window) {										// takes the window as input together with a key
+void processInput(GLFWwindow *window)
+{
 	// checks whether the user has pressed the escape key
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
 
-std::string loadShaderSrc(const char* filename){
+std::string loadShaderSrc(const char *filename)
+{
 	std::ifstream file;
 	std::stringstream buf;
 	std::string ret = "";
-	file.open(filename); //TODO (5) I/O
-	if(file.is_open()) {
+	file.open(filename);
+	if (file.is_open())
+	{
 		buf << file.rdbuf();
 		ret = buf.str();
-	} else {
+	}
+	else
+	{
 		std::cout << "Could not open... " << filename << std::endl;
 	}
-	file.close(); 
+	file.close();
 	return ret;
 }
-
-
